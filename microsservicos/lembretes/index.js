2cb9b21dfe3ccd36b0e997d55ef2be41f8c68db2
Gabriel <gabricouto2305@gmail.com>
2023-05-13 09:14:01 -0300

    Barramento de Eventos Iniciado

--- microsservicos/lembretes/index.js
+++ microsservicos/lembretes/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const axios = require("axios")
 const app = express()
 
 app.use(express.json())
@@ -13,6 +14,11 @@ app.post("/lembretes", (req, res) => {
   const texto = req.body.texto
   id++
   lembretes[id] = {id, texto}
+  axios.post(
+    "http://localhost:10000/eventos", {
+      tipo: "LembreteCriado",
+      dados: {id: id, texto: texto}
+    })
   res.status(201).json(lembretes[id])
 })
 
@@ -21,5 +27,9 @@ app.get("/lembretes", (req, res) => {
   res.send(lembretes) 
 })
 
+app.post("/eventos", (req, res) => {
+  console.log(req.body)
+  res.end()
+})
 const PORT = process.env.PORT || 4000
 app.listen(PORT || 4000, () => console.log(`Lembretes. Porta ${PORT}`))
